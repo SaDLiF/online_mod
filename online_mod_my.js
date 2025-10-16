584d709b172fa2620856192d93fa291b5e904af3
SaDLiF <48650033+SaDLiF@users.noreply.github.com>
2025-10-16 22:18:34 +0300

    Add files via upload

--- online_mod_my.js
+++ online_mod_my.js
@@ -2553,49 +2553,10 @@
        * Показать файлы
        */
 
-        // 1. Добавь эту функцию
-        function getStreamSync(elem) {
-            if (elem.stream && elem.qualitys) {
-                return {
-                    quality: elem.qualitys,
-                    file: elem.stream,
-                    subtitles: elem.subtitles
-                };
-            }
-            return {
-                quality: [],
-                file: '',
-                subtitles: []
-            };
-        }
-
         function append(items) {
             component.reset();
             var viewed = Lampa.Storage.cache('online_view', 5000, []);
             var last_episode = component.getLastEpisode(items);
-
-            // В начале функции append ДО items.forEach
-            function preloadAllStreams(items) {
-                var promises = items.map(function(elem) {
-                    return new Promise(function(resolve) {
-                        if (elem.stream) {
-                            resolve(); // Уже загружено
-                            return;
-                        }
-                        getStream(elem, function() {
-                            resolve(); // Загрузили
-                        }, function() {
-                            resolve(); // Ошибка, но все равно разрешаем
-                        });
-                    });
-                });
-                return Promise.all(promises);
-            }
-
-            preloadAllStreams(items).then(function() {
-                // Теперь все данные готовы
-            });
-
             items.forEach(function(element) {
                 if (element.season) {
                     element.translate_episode_end = last_episode;
@@ -2632,61 +2593,98 @@
                         };
                         Lampa.Player.play(first);
 
-                        var defaultPlayer = Lampa.Storage.get('player_select', 'builtin');
+                        var defaultPlayer = Lampa.Storage.get('player_default');
                         // 'native', 'mx', 'external' и т.д.
 
                         if (element.season) {
-                            var playlist = [];
-                            items.forEach(function(elem) {
-                                if (elem == element) {
-                                    playlist.push(first);
-                                } else {
-                                    
-                                    var ex = getStreamSync(elem); // Синхронно получаем данные
-                                        playlist.push({
-                                            url: component.getDefaultQuality(ex.quality, ex.file),
-                                            quality: component.renameQualityMap(ex.quality),
+                            if (defaultPlayer == 'builtin2') {
+                                // Для встроенного плеера - оригинальная логика
+                                var playlist = [];
+                                items.forEach(function(elem) {
+                                    if (elem == element) {
+                                        playlist.push(first);
+                                    } else {
+                                        var cell = {
+                                            url: function(call) {
+                                                getStream(elem, function(elem) {
+                                                    cell.url = component.getDefaultQuality(elem.qualitys, elem.stream);
+                                                    cell.quality = component.renameQualityMap(elem.qualitys);
+                                                    cell.subtitles = elem.subtitles;
+                                                    call();
+                                                }, function() {
+                                                    cell.url = '';
+                                                    call();
+                                                });
+                                            },
                                             timeline: elem.timeline,
                                             title: elem.title
-                                        });
+                                        };
+                                        playlist.push(cell);
+                                    }
+                                });
+                                Lampa.Player.play(first);
+                                Lampa.Player.playlist(playlist);
+                            } else {
+                                // Для внешнего плеера - загружаем ВСЕ серии перед передачей
+                                var promises = [];
+                                var playlistItems = [];
+                                
+                                // Создаем промисы для всех серий
+                                items.forEach(function(elem, index) {
+                                    var promise = new Promise(function(resolve) {
+                                        if (elem === element) {
+                                            // Текущая серия - уже загружена
+                                            resolve({
+                                                url: elem.url,
+                                                quality: elem.quality,
+                                                subtitles: elem.subtitles,
+                                                timeline: elem.timeline,
+                                                title: elem.title
+                                            });
+                                        } else {
+                                            // Остальные серии - загружаем
+                                            getStream(elem, function(res) {
+                                                resolve({
+                                                    url: component.getDefaultQuality(res.qualitys, res.stream),
+                                                    quality: component.renameQualityMap(res.qualitys),
+                                                    subtitles: res.subtitles,
+                                                    timeline: elem.timeline,
+                                                    title: elem.title
+                                                });
+                                            }, function() {
+                                                resolve({
+                                                    url: '',
+                                                    timeline: elem.timeline,
+                                                    title: elem.title,
+                                                    quality: null,
+                                                    subtitles: null
+                                                });
+                                            });
+                                        }
+                                    });
+                                    promises.push(promise);
+                                });
 
-                                    // if (defaultPlayer == 'builtin') {
-
-                                    //     var cell = {
-                                    //         url: function url(call) {
-                                    //             getStream(elem, function(elem) {
-                                    //                 cell.url = component.getDefaultQuality(elem.qualitys, elem.stream);
-                                    //                 cell.quality = component.renameQualityMap(elem.qualitys);
-                                    //                 cell.subtitles = elem.subtitles;
-                                    //                 call();
-                                    //             }, function() {
-                                    //                 cell.url = '';
-                                    //                 call();
-                                    //             });
-                                    //         },
-                                    //         timeline: elem.timeline,
-                                    //         title: elem.title
-                                    //     };
-
-                                    //     playlist.push(cell);
-                                    // } else {
-
-                                    //     var ex = getStream(elem); // Синхронно получаем данные
-                                    //     playlist.push({
-                                    //         url: component.getDefaultQuality(ex.quality, ex.file),
-                                    //         quality: component.renameQualityMap(ex.quality),
-                                    //         timeline: elem.timeline,
-                                    //         title: elem.title
-                                    //     });                                        
-
-                                    // }
-                                }
-                            });
-                            if (playlist.length > 1)
-                                first.playlist = playlist;
-                            Lampa.Player.play(first);
-                            Lampa.Player.playlist(playlist);
+                                // Ждем загрузки ВСЕХ серий
+                                Promise.all(promises).then(function(results) {
+                                    // Все данные загружены, формируем плейлист
+                                    var fullPlaylist = results;
+                                    
+                                    // Находим индекс текущей серии для старта
+                                    var currentIndex = items.findIndex(function(elem) {
+                                        return elem === element;
+                                    });
+                                    
+                                    // Передаем полный плейлист
+                                    Lampa.Player.play(fullPlaylist[currentIndex]);
+                                    Lampa.Player.playlist(fullPlaylist);
+                                    
+                                    console.log('Передан плейлист из', fullPlaylist.length, 'серий');
+                                });
+                            }
                         } else {
+                            // Для фильмов
+                            Lampa.Player.play(first);
                             Lampa.Player.playlist([first]);
                         }
 
